48eea22d844ae414e649f7331a45425afe1bea46
Jonas Herrmannsdörfer <jonas@herrmannsdoerfer.dev>
2024-12-10 16:40:09 +0100

    12 Inserts

--- src/app/(app)/listing/[listingId]/booking/lib/api/bookListing.ts
+++ src/app/(app)/listing/[listingId]/booking/lib/api/bookListing.ts
@@ -1,8 +1,16 @@
 "use server";
 
 import { ActionResponseType } from "@/lib/types/form";
+import { redirect } from "next/navigation";
+import {
+  Booking,
+  BookingInsertSchema,
+  bookingsTable,
+} from "../db/bookings.sql";
 import { z, ZodFormattedError } from "zod";
-import { Booking, BookingInsertSchema } from "../db/bookings.sql";
+import { db } from "@/lib/db";
+import { listingsTable, usersToBookingsTable } from "@/lib/db/schema";
+import { eq } from "drizzle-orm";
 const today = new Date();
 today.setHours(0, 0, 0, 0);
 
@@ -16,59 +24,128 @@ export const bookListing = async (
   prevState: PrevState,
   formData: FormData,
 ): Promise<PrevState> => {
+  let bookingId: number;
+  let listingId: number;
   const userId = prevState.userId;
-  const payload = Object.fromEntries(formData.entries());
-  const adjustedPayload = {
-    ...payload,
-  };
-  const {
-    data: parsedFormData,
-    success,
-    error,
-  } = BookingInsertSchema.omit({
-    totalPrice: true,
-    guests: true,
-    listingId: true,
-    status: true,
-  })
-    .merge(
-      z.object({
-        guests: z.coerce.number().min(1),
-        listingId: z.coerce.number(),
-        checkIn: z.coerce.date().refine((date) => date >= today, {
-          message: "Check-in Datum muss heute oder in der Zukunft liegen.",
-        }),
-        checkOut: z.coerce.date().refine((date) => date >= today, {
-          message: "Check-out Datum muss heute oder in der Zukunft liegen.",
-        }),
-      }),
-    )
-    .superRefine(({ checkIn, checkOut }, ctx) => {
-      if (checkOut <= checkIn) {
-        ctx.addIssue({
-          code: z.ZodIssueCode.custom,
-          message: "Check-out Datum muss in der Zukunft von Check-in liegen.",
-          fatal: true,
-          path: ["checkOut"],
-        });
-      }
+  try {
+    const payload = Object.fromEntries(formData.entries());
+    const adjustedPayload = {
+      ...payload,
+    };
+    const {
+      data: parsedFormData,
+      success,
+      error,
+    } = BookingInsertSchema.omit({
+      totalPrice: true,
+      guests: true,
+      listingId: true,
+      status: true,
     })
-    .safeParse(adjustedPayload);
+      .merge(
+        z.object({
+          guests: z.coerce.number().min(1),
+          listingId: z.coerce.number(),
+          checkIn: z.coerce.date().refine((date) => date >= today, {
+            message: "Check-in Datum muss heute oder in der Zukunft liegen.",
+          }),
+          checkOut: z.coerce.date().refine((date) => date >= today, {
+            message: "Check-out Datum muss heute oder in der Zukunft liegen.",
+          }),
+        }),
+      )
+      .superRefine(({ checkIn, checkOut }, ctx) => {
+        if (checkOut <= checkIn) {
+          ctx.addIssue({
+            code: z.ZodIssueCode.custom,
+            message: "Check-out Datum muss in der Zukunft von Check-in liegen.",
+            fatal: true,
+            path: ["checkOut"],
+          });
+        }
+      })
+      .safeParse(adjustedPayload);
 
-  if (!success) {
-    const formattedErrors = error.format();
+    if (!success) {
+      const formattedErrors = error.format();
+      return {
+        type: ActionResponseType.error,
+        errors: formattedErrors,
+        userId,
+      };
+    }
+
+    listingId = parsedFormData.listingId;
+    const totalPrice = await getTotalPrice({
+      checkIn: parsedFormData.checkIn,
+      checkOut: parsedFormData.checkOut,
+      listingId,
+    });
+    // TODO: insert booking und insert usersToBookingsTable sollte nur gemeinsam passieren.
+    // das erreicht man mit Transactions
+    // diese lernen wir noch
+    const insertData = {
+      ...parsedFormData,
+      totalPrice,
+    };
+    const booking = await db
+      .insert(bookingsTable)
+      // @ts-expect-error next-line type is correct and parsed
+      .values(insertData)
+      .returning({ bookingId: bookingsTable.id });
+    bookingId = booking[0].bookingId;
+
+    const insertUserToBooking = {
+      // TODO: wenn wir Authentication eingebaut haben, können wir die statische userId entfernen
+      userId: 1,
+      bookingId,
+    };
+    await db.insert(usersToBookingsTable).values(insertUserToBooking);
+  } catch (error) {
+    console.error(error);
     return {
       type: ActionResponseType.error,
-      errors: formattedErrors,
+      errors: { _errors: ["Ferienwohnung konnte nicht gebucht werden."] },
       userId,
     };
   }
 
-  console.log(parsedFormData);
+  redirect(`/listing/${listingId}/booking/${bookingId}`);
+};
 
-  return {
-    type: ActionResponseType.error,
-    errors: { _errors: ["Ferienwohnung konnte nicht gebucht werden."] },
-    userId,
-  };
+const getTotalPrice = async ({
+  checkIn,
+  checkOut,
+  listingId,
+}: {
+  checkIn: Date;
+  checkOut: Date;
+  listingId: number;
+}) => {
+  const durationOfStay = getDurationBetweenTwoDates({
+    firstDate: new Date(checkIn),
+    secondDate: new Date(checkOut),
+  });
+
+  const listing = await db
+    .select({ price: listingsTable.price })
+    .from(listingsTable)
+    .where(eq(listingsTable.id, listingId));
+  const pricePerNight = listing[0].price;
+  return durationOfStay * pricePerNight;
+};
+
+const getDurationBetweenTwoDates = ({
+  firstDate,
+  secondDate,
+}: {
+  firstDate: Date;
+  secondDate: Date;
+}) => {
+  const differenceInTime = firstDate.getTime() - secondDate.getTime();
+  const differenceInDays = Math.abs(
+    Math.round(differenceInTime / (1000 * 3600 * 24)),
+  );
+
+  return differenceInDays;
 };
