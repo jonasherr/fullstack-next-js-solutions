3de5d7401f0038fc76b9ba1a48655a1140389d74
Jonas Herrmannsdörfer <jonas@herrmannsdoerfer.dev>
2024-12-10 11:03:13 +0100

    10 Zod Env Variablen

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,5 +1,5 @@
-import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "./schema";
+import { env } from "../env";
 
-export const db = drizzle(process.env.DATABASE_URL!, { schema });
+export const db = drizzle(env.DATABASE_URL, { schema });
---
+++ src/lib/env.ts
@@ -0,0 +1,9 @@
+import "dotenv/config";
+import { z } from "zod";
+
+const EnvSchema = z.object({
+  DATABASE_URL: z.string(),
+});
+
+export const env = EnvSchema.parse(process.env);
+console.log({ env });
